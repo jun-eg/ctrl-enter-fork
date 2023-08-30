103bb63081e3b055ea3de2651121181895944988
s1f10230014 <s1f102300144@iniad.org>
2023-08-31 00:07:14 +0900

    add:入力をurlに限定,それ以外の場合alertを表示

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -6,10 +6,15 @@ const OptionsIndex = () => {
   const [datas, setData] = useStorage<string[]>('saveDeta', [])
   const [dataInput, setDataInput] = useState('')
 
+  const urlPattern =
+    /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
+
   const addData = async () => {
-    if (dataInput) {
+    if (urlPattern.test(dataInput)) {
       await setData([...datas, dataInput])
-      setDataInput('') // インプットをクリア
+      setDataInput('')
+    } else {
+      alert('URLを入力してください')
     }
   }
 
@@ -18,11 +23,13 @@ const OptionsIndex = () => {
       <div>
         <h1>除外サイト登録</h1>
         <input
+          type="text"
           onChange={(e) => setDataInput(e.target.value)}
           value={dataInput}
         />
         <button onClick={addData}>データ追加</button>
       </div>
+      <h2>登録済み一覧</h2>
       <div>
         {datas.map((data, i) => (
           <div key={i}>{data}</div>
