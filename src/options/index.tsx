b4dfeeafc8cb264f98c9e9e204b3ed707b1bee07
s1f10230014 <s1f102300144@iniad.org>
2023-08-31 01:29:38 +0900

    add:登録済み一覧urlの左側にiconを配置

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 
 import { useStorage } from '@plasmohq/storage/hook'
 
+import styles from './index.module.css'
+
 const OptionsIndex = () => {
   const [datas, setData] = useStorage<string[]>('saveDeta', [])
   const [dataInput, setDataInput] = useState('')
 
   const urlPattern =
     /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
+
+  const getDomain = (data: string) => {
+    const domainPattern = /https?:\/\/[^/]+/
+    const domain = data.match(domainPattern)
+    return domain ? domain[0] : ''
+  }
 
   const addData = async () => {
     if (urlPattern.test(dataInput)) {
@@ -16,6 +24,11 @@ const OptionsIndex = () => {
     } else {
       alert('URLを入力してください')
     }
+  }
+
+  const removeData = (i: number) => {
+    datas.splice(i - 1, 1)
+    setData(datas)
   }
 
   return (
@@ -30,9 +43,14 @@ const OptionsIndex = () => {
         <button onClick={addData}>データ追加</button>
       </div>
       <h2>登録済み一覧</h2>
-      <div>
+
+      <div className={styles.urlListRow}>
         {datas.map((data, i) => (
-          <div key={i}>{data}</div>
+          <div key={i}>
+            <img src={`${getDomain(data)}/favicon.ico`} />
+            <a href={data}>{data}</a>
+            <button onClick={() => removeData(i)}>remove</button>
+          </div>
         ))}
       </div>
     </div>
